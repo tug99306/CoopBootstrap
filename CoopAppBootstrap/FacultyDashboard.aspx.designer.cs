0caa9eda258ea4259d76cbc3192b1a442c49e37e
Saleem Hussein <tug99306@temple.edu>
2021-12-01 00:19:18 -0500

    typo and split term search

--- CoopAppBootstrap/FacultyDashboard.aspx.designer.cs
+++ CoopAppBootstrap/FacultyDashboard.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace CoopAppBootstrap
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ddlSemester control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSemester;
+
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
